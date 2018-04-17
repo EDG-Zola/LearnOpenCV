a16ef6a74101b25998b7e1093f4735f6498726b7
EDG-Zola <wellzengwei@163.com>
2018-04-17 20:31:07 +0800

    Sobel算子

---
+++ CH7/SobelAlgorithm/SobelAlgorithm/main.cpp
@@ -0,0 +1,37 @@
+/************************************************************************/
+/*		此程序演示了运用Sobel算子进行X方向和Y方向、X和Y方向的效果       */
+/************************************************************************/
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main(){
+	//定义Sobel算子要输出的Mat类型
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y, dst;
+	//1.载入原图并显示
+	Mat srcImage = imread("1.jpg");
+	if(!srcImage.data){
+		cout << "打开图片1.jpg失败" << endl;
+		return false;
+	}
+	imshow("原图", srcImage);
+	//2.调用Sobel()函数进行X方向导数（1， 0， 3），默认没有应用缩放
+	// calculates absolute values, and converts the result to 8-bit.
+	Sobel(srcImage, grad_x, CV_16S, 1, 0, 3, 1, 1, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+	imshow("X方向导数", abs_grad_x);
+	//3.调用Sobel()函数进行Y方向导数（0， 1， 3），默认没有应用缩放
+	// calculates absolute values, and converts the result to 8-bit.
+	Sobel(srcImage, grad_y, CV_16S, 0, 1, 3, 1, 1, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y);
+	imshow("Y方向导数", abs_grad_y);
+	//4.整合X和Y方向
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+	imshow("X和Y方向", dst);
+	waitKey(0);
+	return 0;
+}
